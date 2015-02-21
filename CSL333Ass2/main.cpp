a45f5333459f0ab68756b1d4e524f0b8576a5b43
Robin Malhotra <malhotra.robin@yahoo.co.in>
2015-02-21 16:09:00 +0530

    Initial Commit

---
+++ CSL333Ass2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSL333Ass2
+//
+//  Created by Robin Malhotra on 21/02/15.
+//  Copyright (c) 2015 Lal and Robin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
